aeb79f01b3a8b59c199154e040497e58f2886602
CornetS28 <corsam28@gmail.com>
2020-03-19 11:49:50 -0400

    find missing letter solution

---
+++ find_missing_letter.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Find the missing letter
+
+# Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+# You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+# The array will always contain letters in only one case.
+
+# Example:
+
+# ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+# ["a","b","c","d","f"] -> "e"
+# ["O","Q","R","S"] -> "P"
+# (Use the English alphabet with 26 letters!)
+
+def find_missing_letter(arr) # 1st way
+  ([*arr.first..arr.last] - arr).join
+end
+
+def find_missing_letter(arr) # 2nd way
+  ((arr.first..arr.last).to_a - arr).join
+end
+
+def find_missing_letter(arr) # 3rd way
+  arr.each { |letter| return letter.next unless arr.include?(letter.next) }
+end
+
+p find_missing_letter(%w[a b c d f])

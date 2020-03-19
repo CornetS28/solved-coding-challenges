50cfabcfe7fc827cd803aef760b69cff1785d969
CornetS28 <corsam28@gmail.com>
2020-03-19 13:40:04 -0400

    panagram solutions

---
+++ ruby/panagram.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+def panagram?(string)
+  (('a'..'z').to_a - string.downcase.gsub(/[^a-zA-Z]/, '').split.map(&:chars).flatten).empty?
+end
+
+def panagram?(string)
+  ("a".."z").to_a == string.downcase.gsub( /[^a-zA-Z]/, "").split("").sort.uniq
+end
+
+puts panagram?('The quick brown fox jumps over the lazy dog.')

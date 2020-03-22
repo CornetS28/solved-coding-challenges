8ea3ae53e8d5d232faf37dba0b81ea09ea675adf
CornetS28 <corsam28@gmail.com>
2020-03-22 11:34:35 -0400

    fibonicci sequence solution 2 and 3

--- ruby/fibonic.rb
+++ ruby/fibonic.rb
@@ -36,14 +36,14 @@
 # productFib(714) # should return {21, 34, true}, 
 # productFib(800) # should return {34, 55, false}, 
 
-# def fibonacci(num)
-#   num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2) 
-# end
+def fibonacci(num)
+  num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2) 
+end
 
-# def fibonacci(num)
-#   return  num if (0..1).include? num
-#   fibonacci(num - 1) + fibonacci(num -2)
-# end
+def fibonacci(num)
+  return  num if (0..1).include? num
+  fibonacci(num - 1) + fibonacci(num -2)
+end
 
 
 def fibonacci(num)

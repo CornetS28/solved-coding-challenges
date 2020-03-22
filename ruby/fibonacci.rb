3c7bf7d8e18c4f935cea90366e8122d90c727a45
CornetS28 <corsam28@gmail.com>
2020-03-22 17:30:24 -0400

    isPrime solution

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
 
 # such as
@@ -21,32 +23,34 @@
 # Some Examples of Return:
 # (depend on the language)
 
-# productFib(714) # should return (21, 34, true), 
+# productFib(714) # should return (21, 34, true),
 #                 # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
 
-# productFib(800) # should return (34, 55, false), 
+# productFib(800) # should return (34, 55, false),
 #                 # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
 # -----
-# productFib(714) # should return [21, 34, true], 
-# productFib(800) # should return [34, 55, false], 
+# productFib(714) # should return [21, 34, true],
+# productFib(800) # should return [34, 55, false],
 # -----
-# productFib(714) # should return {21, 34, 1}, 
-# productFib(800) # should return {34, 55, 0},        
+# productFib(714) # should return {21, 34, 1},
+# productFib(800) # should return {34, 55, 0},
 # -----
-# productFib(714) # should return {21, 34, true}, 
-# productFib(800) # should return {34, 55, false}, 
+# productFib(714) # should return {21, 34, true},
+# productFib(800) # should return {34, 55, false},
 
 def fibonacci(num)
-  num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2) 
+  num <= 1 ? num : fibonacci(num - 1) + fibonacci(num - 2)
 end
 
 def fibonacci(num)
-  return  num if (0..1).include? num
-  fibonacci(num - 1) + fibonacci(num -2)
-end
+  return num if (0..1).include? num
 
+  fibonacci(num - 1) + fibonacci(num - 2)
+end
 
 def fibonacci(num)
-    ((0..1).include?(num)) ? num :  fibonacci(num - 1) + fibonacci(num -2)
+  (0..1).include?(num) ? num : fibonacci(num - 1) + fibonacci(num - 2)
 end
+
 puts fibonacci(5)
+
